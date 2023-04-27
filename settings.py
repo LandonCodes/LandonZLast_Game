0f94fc960a56ccb641564031d7743edfce62067e
Landon Zafiropoulo <58679340+LandonCodes@users.noreply.github.com>
2023-04-27 09:15:12 -0700

    creating plateform

---
+++ settings.py
@@ -0,0 +1,9 @@
+# Code created by Landon Zafiropoulo
+WIDTH = 500
+HEIGHT = 500
+# Colors
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+FPS = 30
+# Moving Platform
+PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40, (200,200,200), "base"),]
---
+++ sprites.py
@@ -0,0 +1,19 @@
+# Code created by Landon Zafiropoulo
+import pygame as pie
+from pygame.sprite import Sprite
+
+vec = pie.math.Vector2
+
+class Platform(Sprite):
+    def __init__(self, x, y, width, height, color, variant):
+        Sprite.__init__(self)
+        self.width = width
+        self.height = height
+        self.image = pie.Surface((self.width,self.height))
+        self.color = color
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.variant = variant
+        
