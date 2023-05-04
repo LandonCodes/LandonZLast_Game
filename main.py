b5ece25b6146d8090531a9653b222a13a0afec8f
Landon Zafiropoulo <58679340+LandonCodes@users.noreply.github.com>
2023-05-04 12:05:19 -0700

    Changing to working code from last game

--- main.py
+++ main.py
@@ -1,41 +1,127 @@
 # Code created by Landon Zafiropoulo
-
+# Starting from working code
 '''
 create a breakout gameself
 create a restart button
 
 '''
-import pygame as pie
+
+# import libs
+import pygame as pg
 import os
+import sys
+# import settings 
 from settings import *
 from sprites import *
+# from pg.sprite import Sprite
 
 
+
+# set up assets folders
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, "img")
+
+# create game class in order to pass properties to the sprites file
 
 class Game:
     def __init__(self):
-        self.screen = pie.display.set_mode((WIDTH, HEIGHT))
-        pie.display.set_caption("Breakout")
-        self.screen.fill(WHITE)
-        self.clock = pie.time.Clock()
+        # init game window etc.
+        pg.init()
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption("my game")
+        self.clock = pg.time.Clock()
         self.running = True
-    def new(self):
+        #sets score to zero when the game starts
         self.score = 0
-        self.all_sprites = pie.sprite.Group()
-        self.platforms = pie.sprite.Group()
-        self.plat1 = Platform(WIDTH, 50, 0, HEIGHT-50, (150,150,150), "base")
+        print(self.screen)
+    def new(self):
+        # starting a new game
+        self.score = 0
+        self.all_sprites = pg.sprite.Group()
+        self.platforms = pg.sprite.Group()
+        self.enemies = pg.sprite.Group()
+        self.player = Player(self)
+        self.plat1 = Platform(WIDTH, 50, 0, HEIGHT-50, (150,150,150), "normal")
         self.all_sprites.add(self.plat1)
 
+        self.platforms.add(self.plat1)
+        
+        self.all_sprites.add(self.player)
+        for plat in PLATFORM_LIST:
+            p = Platform(*plat)
+            self.all_sprites.add(p)
+            self.platforms.add(p)
+        for i in range(0,10):
+            m = Mob(20,20,(0,255,0))
+            self.all_sprites.add(m)
+            self.enemies.add(m)
+        self.run()
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+    
+    def events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    self.player.jump()
+    def update(self):
+        self.all_sprites.update()
+        #when the player collides with the enimies it reconizges that they collided and adds +1 point to score
+        mhits = pg.sprite.spritecollide(self.player, self.enemies, True)
+        if mhits:
+            #adds the point
+            self.score += 1
+           # print(self.score)
+        if self.score > 8 :
+            mhits = self.score = 10
+            print("You Win!")
 
+        if self.player.vel.y > 0:
+            hits = pg.sprite.spritecollide(self.player, self.platforms, False)
+            if hits:
+                if hits[0].variant == "disappearing":
+                    hits[0].kill()
+                elif hits[0].variant == "bouncey":
+                    self.player.pos.y = hits[0].rect.top
+                    self.player.vel.y = -PLAYER_JUMP
+                else:
+                    self.player.pos.y = hits[0].rect.top
+                    self.player.vel.y = 0
+
+  
+
+    def draw(self):
+        self.screen.fill(BLUE)
+        #prints score color white but only score does not update the numbers
+        self.draw_text(str(self.score), 30, WHITE, WIDTH/2, HEIGHT/2)
+        #self.draw_text(str(self.score), 30, WHITE, WIDTH/2, HEIGHT/2)
+        self.all_sprites.draw(self.screen)
+        # is this a method or a function?
+        pg.display.flip()
+    def draw_text(self, text, size, color, x, y):
+        font_name = pg.font.match_font('arial')
+        font = pg.font.Font(font_name, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x,y)
+        self.screen.blit(text_surface, text_rect)
+    def get_mouse_now(self):
+        x,y = pg.mouse.get_pos()
+        return (x,y)
 g = Game()
 
-
+# kick off the game loop
 while g.running:
     g.new()
-pie.quit()
 
-# running = True
-# while running:
-#     for event in pie.event.get():
-#         if event.type == pie.QUIT:
-#             running = False
+pg.quit()
--- settings.py
+++ settings.py
@@ -1,15 +1,31 @@
-# Code created by Landon Zafiropoulo
-WIDTH = 500
-HEIGHT = 500
-# Colors
+# FIle create by Landon Zafiropoulo
+
+########## screen dimensions #############
+WIDTH = 800
+HEIGHT = 600
+
+############# game settings ##############
+FPS = 30
+RUNNING = True
+SCORE = 0
+PAUSED = False
+
+############ player attributes ###########
+PLAYER_ACC  = 2
+PLAYER_FRICTION = -0.12
+MOB_FRICTION = -0.7
+MOB_ACC= 0.7
+PLAYER_GRAV = 1
+PLAYER_JUMP = 20
+################ colors ##################
 BLACK = (0,0,0)
 WHITE = (255,255,255)
-FPS = 30
-PLAYER_ACC = 2
-PLAYER_FRICTION = -0.3
-PLAYER_JUMP = 20
-PLAYER_GRAV = 0.8
-# Moving Platform
-PLATFORM_LIST = [(10, HEIGHT - 40, WIDTH, 40, (200,200,200), "base"),
-                 (1, HEIGHT - 20, WIDTH, 20, (250,250,200), "base"),
-                 (5, HEIGHT - 10, WIDTH, 30, (300,200,200), "base"),]
+BLUE = (50,50,255)
+RED = (255,50,50)
+########## Starting platforms ############
+PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40, (200,200,200), "normal"),
+                 (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20, (100,255,100), "bouncey"),
+                 (125, HEIGHT - 350, 100, 5, (200,100,50), "normal"),
+                 (350, 200, 100, 20, (200,200,200), "normal"),
+                 (175, 400, 70, 20, (200,200,200), "moving"),]
+
