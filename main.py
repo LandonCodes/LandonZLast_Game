878255bd349928807e9b866f313c2d4ee2d1a8e4
Landon Zafiropoulo <58679340+LandonCodes@users.noreply.github.com>
2023-05-04 09:14:15 -0700

    fixing background

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Code created by Landon Zafiropoulo
 
 '''
-create a breakout game
+create a breakout gameself
 create a restart button
 
 '''
@@ -10,13 +10,34 @@ import os
 from settings import *
 from sprites import *
 
-screen = pie.display.set_mode((WIDTH, HEIGHT))
-pie.display.set_caption("Breakout")
-screen.fill(BLACK)
-clock = pie.time.Clock()
 
-running = True
-while running:
-    for event in pie.event.get():
-        if event.type == pie.QUIT:
-            running = False
+
+class Game:
+    def __init__(self):
+        self.screen = pie.display.set_mode((WIDTH, HEIGHT))
+        pie.display.set_caption("Breakout")
+        self.screen.fill(BLACK)
+        self.clock = pie.time.Clock()
+        self.running = True
+    def new(self):
+        self.score = 0
+        self.all_sprites = pie.sprite.Group()
+        self.platforms = pie.sprite.Group()
+        self.plat1 = Platform(WIDTH, 50, 0, HEIGHT-50, (150,150,150), "normal")
+        self.all_sprites.add(self.plat1)
+
+        self.platforms.add(self.plat1)
+        
+
+g = Game()
+
+
+while g.running:
+    g.new()
+pie.quit()
+
+# running = True
+# while running:
+#     for event in pie.event.get():
+#         if event.type == pie.QUIT:
+#             running = False
