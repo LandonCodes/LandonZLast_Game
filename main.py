68629bb6579213f6d4ef00978b16475c53c34f0d
Landon Zafiropoulo <58679340+LandonCodes@users.noreply.github.com>
2023-05-04 08:49:24 -0700

    working background

--- main.py
+++ main.py
@@ -10,26 +10,13 @@ import os
 from settings import *
 from sprites import *
 
-class Game:
-    def __init__(self):
-        pie.init()
-        pie.mixer.init()
-        self.screen = pie.display.set_mode((WIDTH, HEIGHT))
-        pie.display.set_caption("breakout")
-        self.clock = pie.time.Clock()
-        self.running = True
-        print(self.screen)
-    def new(self):
-        self.all_sprites = pie.sprite.Group()
-        self.platforms = pie.sprite.Group()
-        self.plat0 = Platform(WIDTH, 50, 0, HEIGHT-50, (100,150,150), "base")
-        self.all_sprites.add(self.plat0)
+screen = pie.display.set_mode((WIDTH, HEIGHT))
+pie.display.set_caption("Breakout")
+screen.fill(BLACK)
+clock = pie.time.Clock()
 
-
-        
-g = Game()
-
-while g.running:
-    g.new()
-
-pie.quit()
+running = True
+while running:
+    for event in pie.event.get():
+        if event.type == pie.QUIT:
+            running = False
